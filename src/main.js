17f1ffa472bb67937a37c7cd88f898ff7a0f9bda
bencodes07 <boeckmannben@gmail.com>
2022-05-14 13:19:23 +0200

    Commited on 14/05/22

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 require("dotenv").config()
 const fs = require("fs")
-const { Client, Collection } = require("discord.js")
+const { Client, Collection, Intents } = require("discord.js")
 
-const client = new Client({intents:[]})
-
+const client = new Client({ intents:[Intents.FLAGS.GUILDS] })
 client.commands = new Collection() 
-
 const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js')); 
+
+
 
 commandFiles.forEach((commandFile) => {
 	const command = require(`./commands/${commandFile}`);
@@ -14,32 +14,36 @@ commandFiles.forEach((commandFile) => {
 })
 
 client.once("ready", () => {
-    console.log(`Ready! Logged in as ${client.user.tag}! I'm on ${client.guilds.cache.size} guild(s)!`)
-    client.user.setActivity({name: "mit dem Code", type: "PLAYING"})
+  console.log(`Ready! Logged in as ${client.user.tag}! I'm on ${client.guilds.cache.size} guild(s)!`)
+  client.user.setActivity({name: "mit dem Code", type: "PLAYING"})
 })
 
 client.on("interactionCreate", async (interaction) => {
 
-    if(!interaction.isCommand()) return
+  if(!interaction.isCommand()) return
 
-    const command = client.commands.get(interaction.commandName)
+  const command = client.commands.get(interaction.commandName)
 
-    if(command) {
+  if(command) {
 
-        try {
-            //Wir führen den Command aus
-            await command.execute(interaction);
-        } catch (error) {
+    try {
+      //Wir führen den Command aus
+      await command.execute(interaction);
+    } catch (error) {
 
-            console.error(error);
+      console.error(error);
 
-            if(interaction.deferred || interaction.replied) {
-                interaction.editReply('There was an error while executing this command!')
-            }else {
-                interaction.reply('There was an error while executing this command!')
-            }
-        }
+      if(interaction.deferred || interaction.replied) {
+          interaction.editReply('There was an error while executing this command!')
+      } else {
+          interaction.reply('There was an error while executing this command!')
+      }
     }
+  }
 })
+
+client.on('guildMemberAdd', () => {
+  
+});
 
 client.login(process.env.BOT_TOKEN)
