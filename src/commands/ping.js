26069b120cfeb3e5e6577697e3276d301945e1d7
bencodes07 <boeckmannben@gmail.com>
2022-06-26 23:57:31 +0200

    Commit

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,8 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Interaction } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
+  /** 
+   * @param {Interaction} interaction 
+   */
   async execute(interaction) {
+    if(!interaction.memberPermissions.has("SEND_MESSAGES")) return;
     await interaction.reply(`Pong! Latency: ${interaction.client.ws.ping}ms`);
   }
 }
