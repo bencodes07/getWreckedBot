77ed5bda432138598b6f1e379fc0572e5f94afc7
bencodes07 <boeckmannben@gmail.com>
2022-06-27 00:12:50 +0200

    Commit

--- src/commands/sendRuleAdvisory.js
+++ src/commands/sendRuleAdvisory.js
@@ -2,7 +2,7 @@ const { MessageEmbed, Permissions } = require("discord.js")
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('sendruleadvisory').setDescription('Send the prompt into the rules channel for the users to know what to do'),
+  data: new SlashCommandBuilder().setName('sendruleadvisory').setDescription('Send the prompt into the rules channel for the users to know what to do').setDefaultMemberPermissions(Permissions.FLAGS.ADMINISTRATOR),
   async execute(interaction) {
     // check for permission
     if(interaction.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
