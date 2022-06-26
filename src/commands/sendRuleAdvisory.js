9175531ce0e33b60bd4994be2e4f330f0567e16d
bencodes07 <boeckmannben@gmail.com>
2022-06-27 00:16:03 +0200

    Commit

--- src/commands/sendRuleAdvisory.js
+++ src/commands/sendRuleAdvisory.js
@@ -2,10 +2,10 @@ const { MessageEmbed, Permissions } = require("discord.js")
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('sendruleadvisory').setDescription('Send the prompt into the rules channel for the users to know what to do').setDefaultMemberPermissions(Permissions.FLAGS.ADMINISTRATOR),
+  data: new SlashCommandBuilder().setName('sendruleadvisory').setDescription('Send the prompt into the rules channel for the users to know what to do'),
   async execute(interaction) {
     // check for permission
-    if(interaction.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+    if(interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
       const embed = new MessageEmbed()
       .setTitle("Schreibe ```/rules``` um dir die Regeln anzeigen zu lassen.")
       .setColor('GREEN')
